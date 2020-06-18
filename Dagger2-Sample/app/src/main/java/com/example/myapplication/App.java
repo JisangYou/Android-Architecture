05596319e6e9b042adbdbed31cb932c760a9e170
youjisang <jisangman89@gmail.com>
2020-06-18 23:44:26 +0900

    # update : 아키텍쳐 구조 주석 추가

--- Dagger2-Sample/app/src/main/java/com/example/myapplication/App.java
+++ Dagger2-Sample/app/src/main/java/com/example/myapplication/App.java
@@ -4,6 +4,7 @@ package com.example.myapplication;
 import android.app.Application;
 
 
+// 어플리케이션 상속
 public class App extends Application {
 
     private AppComponent appComponent;
--- Dagger2-Sample/app/src/main/java/com/example/myapplication/MainFragmentModule.java
+++ Dagger2-Sample/app/src/main/java/com/example/myapplication/MainFragmentModule.java
@@ -1,16 +1,6 @@
 package com.example.myapplication;
-
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.util.Log;
-import android.widget.FrameLayout;
-
 import java.util.Random;
 
-import javax.inject.Inject;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
 import dagger.Module;
 import dagger.Provides;
 
