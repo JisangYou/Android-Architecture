92995f408f5fae7e2dd581e400e689c1cc55763f
JisangYou <jisangman89@gmail.com>
2020-06-30 16:54:10 +0900

    # update : subcomponent

--- Chapter2-Sample/app/src/main/java/com/example/myapplication/Inheritance/ComponentB.java
+++ Chapter2-Sample/app/src/main/java/com/example/myapplication/Inheritance/ComponentB.java
@@ -1,7 +1,6 @@
 package com.example.myapplication.Inheritance;
 
 import dagger.Component;
-import dagger.Module;
 
 @Component(modules = ModuleB.class, dependencies = ComponentA.class)
 public interface ComponentB {
