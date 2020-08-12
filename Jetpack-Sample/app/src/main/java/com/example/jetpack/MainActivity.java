bf6e2875e899932e4def52ecd93b9b6f42a8ef69
JisangYou <jisangman89@gmail.com>
2020-08-12 11:06:26 +0900

    # update : 레이아웃에 변수 선언

--- Jetpack-Sample/app/src/main/java/com/example/jetpack/MainActivity.java
+++ Jetpack-Sample/app/src/main/java/com/example/jetpack/MainActivity.java
@@ -26,7 +26,19 @@ public class MainActivity extends AppCompatActivity {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         LinearLayout rootLayout = binding.root;
-        TextView textView = binding.tv;
-        textView.setText("Hello Jay");
+        /**
+         * id로 View 참조하기.
+         */
+//        TextView textView = binding.tv;
+//        textView.setText("Hello Jay");
+        /**
+         * 레이아웃에 변수 선언하기.
+         */
+//        binding.setMyText("Hello Jay~");
+        /**
+         * 포조 클래스 변수 선언
+         */
+        User user = new User("Jay", "ComeOn");
+        binding.setUser(user);
     }
 }
