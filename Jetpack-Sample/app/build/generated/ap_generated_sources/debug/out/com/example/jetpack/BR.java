b31db9bccd131535f3743b3c41ffc16b37e14042
JisangYou <jisangman89@gmail.com>
2020-08-19 20:01:46 +0900

    # fix

---
+++ Jetpack-Sample/app/build/generated/ap_generated_sources/debug/out/com/example/jetpack/BR.java
@@ -0,0 +1,15 @@
+package com.example.jetpack;
+
+public class BR {
+  public static final int _all = 0;
+
+  public static final int index = 1;
+
+  public static final int key = 2;
+
+  public static final int list = 3;
+
+  public static final int map = 4;
+
+  public static final int sparse = 5;
+}
