5594fc8973d5132e1a33cc196c1e7d55fd4c5716
youjisang <jisangman89@gmail.com>
2020-08-30 22:05:04 +0900

    # refactoring : workmanager

--- Login-Test-Sample/app/src/main/java/com/example/myapplication/module/SubcomponentsModule.java
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication.module;
-
-import com.example.myapplication.component.LoginComponent;
-
-import dagger.Module;
-
-@Module(subcomponents = LoginComponent.class)
-public class SubcomponentsModule {
-}
