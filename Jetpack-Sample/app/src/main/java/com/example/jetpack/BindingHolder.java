50e106cf1d7c8005be2fdcff51f985b64c6c1eee
JisangYou <jisangman89@gmail.com>
2020-09-06 23:31:34 +0900

    # update: BindingHolder 예제

---
+++ Jetpack-Sample/app/src/main/java/com/example/jetpack/BindingHolder.java
@@ -0,0 +1,11 @@
+package com.example.jetpack;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.ViewDataBinding;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+
+
